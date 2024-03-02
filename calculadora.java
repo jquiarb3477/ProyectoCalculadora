dba0e44d87c63d79471cd1db0adc4b419697c4b8
Juan <juan.camilo.quintero.arboleda@iescelia.org>
2024-03-02 17:52:40 +0100

    Commit inicial Juan

---
+++ calculadora.java
@@ -0,0 +1,13 @@
+public class Calculadora {
+    Private int num1;
+    Private int num2;
+
+    public Calculadora(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int suma() {
+        return this.num1 + this.num2;
+    }
+}
